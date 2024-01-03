acb9d46c76d6ec7e09868c7268f40b6ae4c1199c
Ahmed Amin Hassan <AhmedAminHassanIsmail@gmail.com>
2024-01-03 13:09:43 +0200

    inital commit create xCode project with deployment iOS 15.0

---
+++ Pet Adoption/Pet Adoption/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pet Adoption
+//
+//  Created by Ahmed Amin on 03/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pet Adoption/Pet Adoption/Pet_AdoptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pet_AdoptionApp.swift
+//  Pet Adoption
+//
+//  Created by Ahmed Amin on 03/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Pet_AdoptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
